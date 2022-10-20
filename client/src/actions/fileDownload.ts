c749195c806957f39dd74f110194409e826d4ec6
1lymax <59887205+1lymax@users.noreply.github.com>
2022-10-20 12:41:33 +0300

    feat: file download

---
+++ client/src/actions/fileDownload.ts
@@ -0,0 +1,22 @@
+import {LocalStorage} from "ts-localstorage";
+import {ACCESS_TOKEN} from "../utils/consts";
+import {IFile} from "../models/IFile";
+
+export const fileDownload = async (file: IFile) => {
+    const response = await fetch(`http://localhost:5000/api/files/download?id=${file._id}`, {
+        headers: {
+            Authorization: `Bearer ${LocalStorage.getItem(ACCESS_TOKEN)}`
+        }
+    })
+    if (response.status === 200) {
+        const blob = await response.blob()
+        const downloadUrl = window.URL.createObjectURL(blob)
+        const link = document.createElement('a')
+        link.href = downloadUrl
+        link.download = file.name as string
+        document.body.appendChild(link)
+        link.click()
+        link.remove()
+    }
+    return response
+};
--- client/src/components/disk/file/File.tsx
+++ client/src/components/disk/file/File.tsx
@@ -1,11 +1,16 @@
-import React, {FC} from 'react';
-import {Grid, Tooltip, Typography} from "@mui/material";
+import React, {FC, useState} from 'react';
+import ShareIcon from '@mui/icons-material/Share';
 import FolderOpenIcon from '@mui/icons-material/FolderOpen';
+import DeleteForeverIcon from '@mui/icons-material/DeleteForever';
+import CloudDownloadIcon from '@mui/icons-material/CloudDownload';
+import {Grid, IconButton, Tooltip, Typography} from "@mui/material";
 import TextSnippetOutlinedIcon from '@mui/icons-material/TextSnippetOutlined';
 
 import {IFile} from "../../../models/IFile";
+import {fileDownload} from "../../../actions/fileDownload";
 import {useAppDispatch, useAppSelector} from "../../../hooks/hooks";
 import {popDirStack, pushDirStack, setCurrentDir} from "../../../store/reducers/FileSlice";
+
 
 interface FileProps {
     file: IFile;
@@ -13,7 +18,13 @@ interface FileProps {
 
 const File: FC<FileProps> = ({file}) => {
     const dispatch = useAppDispatch()
+    const [actionButtonsVisible, setActionButtonsVisible] = useState(false)
     const currentDir = useAppSelector(state => state.fileState.currentDir)
+
+    const downloadHandler = async (e: React.MouseEvent<HTMLButtonElement>) => {
+        e.stopPropagation()
+        fileDownload(file)
+    }
 
     const handleDoubleClick = () => {
         if (currentDir === file._id) {
@@ -26,13 +37,16 @@ const File: FC<FileProps> = ({file}) => {
 
     return (
         <Grid container alignItems="center" onDoubleClick={() => handleDoubleClick()}
-              sx={{display: "grid", gridTemplateColumns: "1fr 4fr repeat(4, 1fr)", minHeight: '40px'}}>
+              sx={{display: "grid", gridTemplateColumns: "1fr 4fr 2fr 1fr 1fr", minHeight: '40px'}}
+              onMouseEnter={() => setActionButtonsVisible(true)}
+              onMouseLeave={() => setActionButtonsVisible(false)}
+        >
             <Grid item sx={{gridColumnStart: 1, justifySelf: "center"}}>
                 {file.type === "dir"
                     ?
                     <>
                         {currentDir !== file._id &&
-                            <FolderOpenIcon sx={{fontSize: 40}} color="primary"/>
+							<FolderOpenIcon sx={{fontSize: 40}} color="primary"/>
                         }
                     </>
                     :
@@ -43,7 +57,22 @@ const File: FC<FileProps> = ({file}) => {
                     {file.name}
                 </Typography>
             </Grid>
-            <Grid item sx={{gridColumnStart: 5, justifySelf: "center"}}>
+            <Grid item sx={{gridColumnStart: 3, justifySelf: "right"}}
+                  hidden={!actionButtonsVisible}
+            >
+                {file.type !== "dir" &&
+					<IconButton color='secondary' onClick={(e) => downloadHandler(e)}>
+						<CloudDownloadIcon/>
+					</IconButton>
+                }
+                <IconButton color='secondary'>
+                    <DeleteForeverIcon/>
+                </IconButton>
+                <IconButton color='secondary'>
+                    <ShareIcon/>
+                </IconButton>
+            </Grid>
+            <Grid item sx={{gridColumnStart: 4, justifySelf: "center"}}>
                 <Tooltip title={`${file.date.slice(4, 16)} ${file.date.slice(16, 24)}`}>
                     <Typography variant="subtitle2">
                         {file.date.slice(4, 15)}
@@ -51,7 +80,7 @@ const File: FC<FileProps> = ({file}) => {
                 </Tooltip>
 
             </Grid>
-            <Grid item sx={{gridColumnStart: 6, justifySelf: "center"}}>
+            <Grid item sx={{gridColumnStart: 5, justifySelf: "center"}}>
                 <Typography variant="subtitle1">
                     {file.size ? file.size.toString() : ''}
                 </Typography>
